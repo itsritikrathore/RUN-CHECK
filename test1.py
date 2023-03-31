9c360d022c0222321a1fd5991c30892eef253e4f
itsritikrathore <129522646+itsritikrathore@users.noreply.github.com>
2023-03-31 19:14:40 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,9 @@
+a = 33
+b = 33
+if b > a :
+
+    print("b  > a")
+
+elif a == b :
+   
+   print("b = a")
---
+++ test2.py
@@ -0,0 +1,5 @@
+def my_art(fname):
+    print(fname+'art')
+my_art('ishmeet')
+my_art('ritik')
+my_art('mam')
